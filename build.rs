b53a2d38f99f08bb9649035195228dc46be9cc7f
sd <sd@netit-services.com>
2023-07-04 16:50:48 +0200

    remove default proto compilation to make docs.rs work

--- build.rs
+++ build.rs
@@ -2,10 +2,10 @@
 // dnf install -y gcc-c++
 
 fn main() {
-    tonic_build::configure()
-        .build_client(true)
-        .build_server(true)
-        .out_dir("src/rpc/")
-        .compile(&["proto/cache.proto"], &["proto"])
-        .expect("Failed to compile proto/cache.proto");
+    // tonic_build::configure()
+    //     .build_client(true)
+    //     .build_server(true)
+    //     .out_dir("src/rpc/")
+    //     .compile(&["proto/cache.proto"], &["proto"])
+    //     .expect("Failed to compile proto/cache.proto");
 }
